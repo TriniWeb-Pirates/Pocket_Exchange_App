74b3e47e1dadb954c0b86b63bd696de3d13a4bac
amandalalman <alalman@yahoo.com>
2023-02-26 13:50:47 +0000

    signup page 1 finished

--- App/views/index.py
+++ App/views/index.py
@@ -4,7 +4,7 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
 @index_views.route('/', methods=['GET'])
 def index_page():
-    return render_template('index.html')
+    return render_template('signup.html')
 
 @index_views.route('/', methods=['GET'])
 def health_check():
