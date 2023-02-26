be602cf4051c97f1e111555d2dd225728e71b56e
reyhanmaha <reyhan12maharaj@gmail.com>
2023-02-26 21:04:35 +0000

    implemented both signup routes

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,8 +1,8 @@
 from App.models import User
 from App.database import db
 
-def create_user(username, password,firstName,lastName,phone,email,city,biography,links):
-    newuser = User(username=username, password=password,firstName=firstName,lastName=lastName,phoneNumber=phone,email=email,city=city,Bio=biography,links=links)
+def create_user(username, password,firstName,lastName,email,phone,city,biography,links):
+    newuser = User(username=username, password=password,firstName=firstName,lastName=lastName,email=email,phoneNumber=phone,city=city,Bio=biography,links=links)
     print(newuser)
     db.session.add(newuser)
     db.session.commit()
