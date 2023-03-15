b85c9f9c9266130e1250588c9fb9fa8a4f7bae8b
reyhanmaha <reyhan12maharaj@gmail.com>
2023-03-15 21:14:38 +0000

    trying the ARRAY column

--- App/models/manager.py
+++ App/models/manager.py
@@ -1,12 +1,13 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
+#from flask_sqlalchemy import SQLAlchemy, ARRAY
 
 class Manager(db.Model):
     __tablename__='manager'
     id = db.Column(db.Integer, primary_key=True)
     lendingOfferID= db.Column(db.Integer,db.ForeignKey('lendingOffer.id'), unique=True, nullable=False)
     InterestedUserList= db.Column(db.Text, nullable=True)
-
+    #InterestedUserList=db.Column(ARRAY(Integer), nullable=True)
     def __init__(self,InterestedUserList):
         self.InterestedUserList=InterestedUserList
 
